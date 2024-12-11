3d5901c0f24ca2aeb22c841189409d8b8aba03d9
Tanvi Shetty <shettytanvi888@gmail.com>
2024-12-11 18:54:29 +0530

    Real_Estate

--- src/components/Companies/Companies.jsx
+++ src/components/Companies/Companies.jsx
@@ -4,7 +4,7 @@ const Companies = () => {
     return (
         <section className='c-wrapper'>
     
-            <div className='/paddings innerWidth flexCenter c-container'>
+            <div className='paddings innerWidth flexCenter c-container'>
     
               <img src="prologis.png" alt="" />
               <img src="tower.png" alt="" />
